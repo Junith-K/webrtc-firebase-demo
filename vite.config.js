75657891788a5488732fd1a227ecbbeffc5676d8
Junith-K <junithjunith28@gmail.com>
2024-11-02 17:29:58 +0530

    add vite config for disable overlay

---
+++ vite.config.js
@@ -0,0 +1,11 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  server: {
+    port: 8080, // Set your desired port here
+    hmr: {
+      overlay: false, // Disable the overlay to prevent unwanted reloads
+    }
+  }
+});
