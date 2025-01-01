960226c4f791957802d4c4dd1ed2216adcc91ce0
Junith-K <junithjunith28@gmail.com>
2025-01-01 19:24:10 +0530

    upadte config

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
 const { defineConfig } = require('vite');
 
 module.exports = defineConfig({
-  base: '/',
+  base: './', // Ensure base path is relative
   build: {
-    outDir: 'dist',
+    outDir: 'dist', // Output directory for build
     rollupOptions: {
       output: {
-        entryFileNames: '[name].js',
-        chunkFileNames: '[name].js',
-        assetFileNames: '[name].[ext]'
-      }
-    }
-  }
+        entryFileNames: '[name].js', // Output entry file name format
+        chunkFileNames: '[name].js', // Output chunk file name format
+        assetFileNames: '[name].[ext]', // Output asset file name format
+      },
+    },
+  },
 });
