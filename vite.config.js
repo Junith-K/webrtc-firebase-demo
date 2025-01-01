6e1ec4616ced9b98a65a339b440579d3abe49160
Junith-K <junithjunith28@gmail.com>
2025-01-01 19:51:03 +0530

    update folders

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
-const { defineConfig } = require('vite');
+import { defineConfig } from 'vite';
 
-module.exports = defineConfig({
-  base: './', // Ensure base path is relative
+export default defineConfig({
   build: {
-    outDir: 'dist', // Output directory for build
-    rollupOptions: {
-      output: {
-        entryFileNames: '[name].js', // Output entry file name format
-        chunkFileNames: '[name].js', // Output chunk file name format
-        assetFileNames: '[name].[ext]', // Output asset file name format
-      },
-    },
+    outDir: 'dist', // Vite will output files here
+    assetsDir: 'assets',
   },
 });
